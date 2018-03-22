9d96ececeb2413796633d1bd5bd64c085bb372f5
linxiaodan <1340787373@qq.com>
2018-03-22 14:45:28 +0800

    判断登录用户等级

--- Supermanage/urls.py
+++ Supermanage/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', view.login),
     url(r'^$', view.login),
+    url(r'stock', view.stock),
 ]
--- Supermanage/view.py
+++ Supermanage/view.py
@@ -7,11 +7,13 @@
    date：          18-3-21
 """
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .model import checkUser
+from settings import cookies_salt
 
 
+# 登录
 def login(request):
     if request.method == 'GET':
         return render(request, 'login.html', {})
@@ -20,9 +22,28 @@ def login(request):
         password = request.POST.get('password')
 
         check_user = checkUser(username, password)
-        if check_user == 0:
-            return HttpResponse('管理员登录成功')
-        elif check_user == 1:
-            return HttpResponse('员工登录成功')
+        if check_user == 0 or check_user == 1:
+            res = redirect('/stock')
+            res.set_cookie('username', username, 60*60*24)
+            res.set_cookie('user_level', check_user, 60*60*24)
+            return res
+
         else:
             return render(request, 'login.html', {'msg': '用户名或密码错误'})
+
+
+# 库存
+def stock(request):
+    username = request.COOKIES.get('username')
+    user_level = int(request.COOKIES.get('user_level'))
+    print user_level
+
+    if user_level == 0 or user_level == 1:
+        print user_level
+        context = {
+            'username': username
+        }
+        return render(request, 'stock.html', context)
+    else:
+        print 'no'
+        return redirect('/login')
